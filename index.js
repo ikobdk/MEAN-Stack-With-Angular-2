5fb5f89db83bbe4d968100a1cc4f68f5298420b6
ikobdk <ikobidike@gmail.com>
2017-08-31 19:03:32 -0600

    server-setup

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const path = require('path')
 
 mongoose.Promise = global.Promise
 mongoose.connect(config.uri,(err)=>{
-  if(err){console.log('could not connect to database: ',err)}
-  else{
+  if(err){
+    console.log('could not connect to database: ',err)
+  }else{
     // console.log(config.secret)
-    // console.log('connected to database: '+ config.db)
+    console.log('connected to database: '+ config.db)
   }
 });
 
 app.use(express.static(__dirname + '/client/dist/'))
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname + '/client/dist/index.html'))
+  res.send(`<h1>I am the best </h1>`)
+  //res.sendFile(path.join(__dirname + '/client/dist/index.html'))
 });
 
 app.listen(8080, ()=>{
-  console.log('Listening on port 8080')
-})
+  console.log('Listening on port 8080');
+});
